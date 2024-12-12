d75125afe7483db97799c076a0bc548cb19ca4fe
matejur <matej.urbas00@gmail.com>
2024-12-12 10:38:36 +0100

    2024 Day 12

--- 2024/solutions/day12.cpp
+++ 2024/solutions/day12.cpp
@@ -9,17 +9,51 @@
 
 using namespace std;
 namespace day12 {
+int dirs[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-int calculateSides(const set<pair<int, int>> &fences) {
-    int corners = 0;
+int countSides(const set<tuple<int, int, int>> &fences) {
+    int sides = 0;
+    set<tuple<int, int, int>> visited;
 
-    return corners;
+    for (auto &fence : fences) {
+        auto [x, y, dir] = fence;
+
+        if (visited.count(fence)) {
+            continue;
+        }
+
+        visited.insert(fence);
+
+        int startX = x;
+        int startY = y;
+
+        // go perpendicular to the fence in both directions
+        int newDir = (dir + 1) % 4;
+        while (fences.count({x, y, dir})) {
+            x += dirs[newDir][0];
+            y += dirs[newDir][1];
+            visited.insert({x, y, dir});
+        }
+
+        x = startX;
+        y = startY;
+        newDir = (dir + 3) % 4;
+        while (fences.count({x, y, dir})) {
+            x += dirs[newDir][0];
+            y += dirs[newDir][1];
+            visited.insert({x, y, dir});
+        }
+
+        sides++;
+    }
+
+    return sides;
 }
 int calculatePrice(const vector<string> &input, int x, int y, bool *visited,
                    bool part2) {
-    set<pair<int, int>> fences;
-    queue<pair<int, int>> q;
-    q.push({x, y});
+    set<tuple<int, int, int>> fences;
+    queue<tuple<int, int, int>> q;
+    q.push({x, y, 0});
 
     char region = input[y][x];
     int area = 0;
@@ -27,13 +61,13 @@ int calculatePrice(const vector<string> &input, int x, int y, bool *visited,
     int corners = 0;
 
     while (!q.empty()) {
-        auto [x, y] = q.front();
+        auto [x, y, dir] = q.front();
         q.pop();
 
         if (x < 0 || x >= input[y].size() || y < 0 || y >= input.size() ||
             input[y][x] != region) {
             perimeter++;
-            fences.insert({x, y});
+            fences.insert({x, y, dir});
             continue;
         }
 
@@ -44,14 +78,13 @@ int calculatePrice(const vector<string> &input, int x, int y, bool *visited,
         visited[y * input[y].size() + x] = true;
         area++;
 
-        q.push({x + 1, y});
-        q.push({x - 1, y});
-        q.push({x, y + 1});
-        q.push({x, y - 1});
+        for (int i = 0; i < 4; i++) {
+            q.push({x + dirs[i][0], y + dirs[i][1], i});
+        }
     }
 
     if (part2) {
-        perimeter = calculateSides(fences);
+        perimeter = countSides(fences);
     }
 
     return area * perimeter;
