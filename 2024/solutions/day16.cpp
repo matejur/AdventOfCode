96ca1ca7d4210c769eacff5cfdaa4dd801119a47
matejur <matej.urbas00@gmail.com>
2024-12-16 14:37:37 +0100

    2024 Day 16

--- 2024/solutions/day16.cpp
+++ 2024/solutions/day16.cpp
@@ -1,5 +1,6 @@
 #include "day16.h"
 
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <queue>
@@ -11,7 +12,7 @@
 using namespace std;
 namespace day16 {
 int dirs[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-int lowestScore(const vector<string> grid) {
+pair<int, int> solve(const vector<string> grid) {
     int startX = 1;
     int startY = grid.size() - 2;
 
@@ -21,7 +22,7 @@ int lowestScore(const vector<string> grid) {
 
     pq.push(make_tuple(0, startX, startY, 0));
 
-    set<tuple<int, int, int>> visited;
+    map<tuple<int, int, int>, int> scores;
 
     while (!pq.empty()) {
         auto [score, x, y, dir] = pq.top();
@@ -31,37 +32,64 @@ int lowestScore(const vector<string> grid) {
             continue;
         }
 
-        if (visited.count(make_tuple(x, y, dir))) {
+        if (scores.count(make_tuple(x, y, dir))) {
             continue;
         }
 
-        visited.insert(make_tuple(x, y, dir));
+        scores[{x, y, dir}] = score;
 
         if (grid[y][x] == 'E') {
-            return score;
+            queue<tuple<int, int, int>> q;
+            q.push({x, y, dir});
+
+            set<pair<int, int>> path;
+
+            while (!q.empty()) {
+                auto curr = q.front();
+                auto [x, y, dir] = curr;
+                q.pop();
+
+                int currScore = scores[curr];
+
+                if (path.count({x, y})) {
+                    continue;
+                }
+                path.insert({x, y});
+
+                for (int d = 0; d < 4; d++) {
+                    int newX = x - dirs[d][0];
+                    int newY = y - dirs[d][1];
+
+                    if (grid[newY][newX] == '#') {
+                        continue;
+                    }
+
+                    if (scores.count({newX, newY, d})) {
+                        int diff = currScore - scores[{newX, newY, d}];
+                        if (diff == 1 || diff == 1001) {
+                            q.push({newX, newY, d});
+                        }
+                    }
+                }
+            }
+
+            return {score, path.size()};
         }
 
         auto forward = dirs[dir];
-        auto left = dirs[(dir + 1) % 4];
-        auto right = dirs[(dir + 3) % 4];
 
-        pq.push(make_tuple(score + 1, x + forward[0], y + forward[1], dir));
-        pq.push(
-            make_tuple(score + 1001, x + left[0], y + left[1], (dir + 1) % 4));
-        pq.push(make_tuple(score + 1001, x + right[0], y + right[1],
-                           (dir + 3) % 4));
+        pq.push(make_tuple(score + 1, x + dirs[dir][0], y + forward[1], dir));
+        pq.push(make_tuple(score + 1000, x, y, (dir + 1) % 4));
+        pq.push(make_tuple(score + 1000, x, y, (dir + 3) % 4));
     }
-    return -1;
-}
-string part1(const vector<string> &input) {
-    return to_string(lowestScore(input));
-}
 
-string part2(const vector<string> &input) { return "Not implemented"; }
+    return {-1, -1};
+}
 
 void run(const vector<string> &input) {
     cout << "----- Day 16 -----" << endl;
-    cout << "Part 1: " << part1(input) << endl;
-    cout << "Part 2: " << part2(input) << endl;
+    auto [part1, part2] = solve(input);
+    cout << "Part 1: " << part1 << endl;
+    cout << "Part 2: " << part2 << endl;
 }
 };  // namespace day16
