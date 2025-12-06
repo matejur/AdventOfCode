ae65d538fe60c9f88dc9906932aaa7d89a883b0f
matejur <matej.urbas00@gmail.com>
2025-12-06 10:03:01 +0100

    2025 Days 4 speedup

---
+++ 2025/src/solutions/template.sh
@@ -0,0 +1,9 @@
+use anyhow::Result;
+
+use crate::day_test;
+
+pub fn solve(input: &str) -> Result<(String, String)> {
+    Ok(("".to_string(), "".to_string()))
+}
+
+day_test!(day06_test, 6, example = ("", ""), input = ("", ""));
