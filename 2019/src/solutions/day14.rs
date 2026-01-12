62921ea7cc5d4db5d8e126092064583f4e884ae2
matejur <matej.urbas00@gmail.com>
2026-01-12 18:51:18 +0100

    2019 day 14 speedup

--- 2019/src/solutions/day14.rs
+++ 2019/src/solutions/day14.rs
@@ -84,51 +84,40 @@ fn topological<'a>(reactions: &HashMap<&'a str, Reaction<'a>>) -> Vec<&'a str> {
         }
     }
 
+    order.reverse();
     order
-}
-
-fn pop_highest_priority<'a>(
-    map: &mut HashMap<&'a str, usize>,
-    order_index: &HashMap<&'a str, usize>,
-) -> Option<(&'a str, usize)> {
-    let (&name, _) = map.iter().max_by_key(|(k, _)| order_index[*k])?;
-    let amount = map.remove(name).unwrap();
-    Some((name, amount))
 }
 
 fn ore_for_n_fuel<'a>(
     n: usize,
     reactions: &HashMap<&'a str, Reaction<'a>>,
-    order_index: &HashMap<&'a str, usize>,
+    order: &Vec<&str>,
 ) -> Result<usize> {
     let mut to_make = HashMap::new();
     to_make.insert("FUEL", n);
 
-    let mut ore_needed = 0;
-    while let Some((name, amount)) = pop_highest_priority(&mut to_make, order_index) {
-        if name == "ORE" {
-            ore_needed += amount;
-            continue;
+    for name in order {
+        let needed = to_make.remove(name).expect("It should be in here!");
+        if *name == "ORE" {
+            return Ok(needed);
         }
 
         let reaction = &reactions[name];
-        let times = div_ceil(amount, reaction.output.amount);
+        let times = div_ceil(needed, reaction.output.amount);
 
         for input in &reaction.input {
             *to_make.entry(input.name).or_insert(0) += input.amount * times;
         }
     }
 
-    Ok(ore_needed)
+    unreachable!("There should be ORE in order!");
 }
 
 pub fn solve(input: &str) -> Result<(String, String)> {
     let reactions = parse(input)?;
     let order = topological(&reactions);
-    let order_index: HashMap<&str, usize> =
-        order.iter().enumerate().map(|(i, &c)| (c, i)).collect();
 
-    let part1 = ore_for_n_fuel(1, &reactions, &order_index)?;
+    let part1 = ore_for_n_fuel(1, &reactions, &order)?;
 
     let threshold: usize = 1_000_000_000_000;
     let mut min = 1;
@@ -136,7 +125,7 @@ pub fn solve(input: &str) -> Result<(String, String)> {
 
     let part2 = loop {
         let mid = (min + max).div_ceil(2);
-        let ore = ore_for_n_fuel(mid, &reactions, &order_index)?;
+        let ore = ore_for_n_fuel(mid, &reactions, &order)?;
 
         if ore < threshold {
             min = mid;
